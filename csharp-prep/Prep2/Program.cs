810e6ab386f68f76d604a361085e77575577f305
CodeOracleClayton <tonie.zulu94@gmail.com>
2024-01-10 16:00:09 +0200

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage? ");
+        string percentageMark = Console.ReadLine();
+        int actualMark = int.Parse(percentageMark);
+
+        string letter = "";
+
+        if (actualMark >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (actualMark >= 80)
+        {
+            letter = "B";
+        }   
+
+        else if (actualMark >= 70)
+        {
+            letter = "C";
+        }    
+
+        else if (actualMark >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "E";
+        }
+
+        Console.WriteLine("Your grade is {0}", letter);
+
+        if (actualMark > 70)
+        {
+            Console.WriteLine("You have passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Try to work harder next time");
+        }
     }
 }
