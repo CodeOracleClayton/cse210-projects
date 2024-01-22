0fd4185ad8ec596cf1badfa6272d64d932d3e3bb
Nana-Koffie <claytonzulu1210@gmail.com>
2024-01-22 09:58:41 +0200

    Getting there

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Formats.Asn1;
+using Learning03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.Write("What is the numerator? ");
+        string answerTop = Console.ReadLine();
+        int top = int.Parse(answerTop);
+
+        Console.Write("What is the numerator? ");
+        string answerBottom = Console.ReadLine();
+        int bottom = int.Parse(answerBottom);
+
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetFractionString()); 
+        Console.WriteLine(fraction.GeDecimalValue());
+
+        Fraction fraction1 = new Fraction(top);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GeDecimalValue());
+
+        Fraction fraction2 = new Fraction(top, bottom);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GeDecimalValue());
     }
 }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
+        Console.WriteLine("Welcome to the scripture memorizer!");
+
+        Scripture myScripture = new Scripture("Proverbs", "3", "5-6", "Trust in the LORD with all your heart and lean not on your own understanding;  in all your ways acknowledge him, and he will make your paths straight.");
+        bool finish = false;
+        string answer;
+        while (finish == false)
+        {
+            Console.WriteLine("Press the Enter key to continue or type quit to finish");
+            answer = Console.ReadLine();
+            myScripture.DisplayRenderedText();
+            myScripture.HideWords();
+            myScripture.IsCompletelyHidden();
+            if (answer == "quit")
+            {
+                finish = true;
+            }
+
+
+        }
     }
 }
