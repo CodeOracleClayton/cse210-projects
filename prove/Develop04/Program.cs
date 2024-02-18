d6ef29d354b729717740da3a6ed0c658ec0a9d99
Nana-Koffie <claytonzulu1210@gmail.com>
2024-02-18 06:36:44 +0200

    Week 4

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,8 +2,57 @@ using System;
 
 class Program
 {
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool error = false;
+
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+
+            if (error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input. Please try again.");
+                Console.ResetColor();
+                error = false;
+            }
+
+            Console.Write("Select a choice from the menu: ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.Run();
+            }
+            else if (input == "2")
+            {
+                ReflectingActivity reflectingActivity = new ReflectingActivity();
+                reflectingActivity.Run();
+            }
+            else if (input == "3")
+            {
+                ListingActivity listingActivity = new ListingActivity();
+                listingActivity.Run();
+            }
+
+            else if (input == "4")
+            {
+                break;
+            }
+            else
+            {
+                error = true;
+            }
+        }
     }
 }
